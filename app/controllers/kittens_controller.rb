ab3de5babdd965d48e092f75b429bfc27c768dc6
John-Rashta <john_rashta@outlook.com>
2026-01-29 15:24:34 +0000

    feat: finish project

--- app/controllers/kittens_controller.rb
+++ app/controllers/kittens_controller.rb
@@ -2,9 +2,20 @@ class KittensController < ApplicationController
   before_action :set_kitten, only: %i[ show edit update destroy ]
   def index
     @kittens = Kitten.all
+
+    respond_to do |format|
+      format.html
+      format.xml { render xml:  @kittens }
+      format.json  { render json: @kittens }
+    end
   end
 
   def show
+    respond_to do |format|
+      format.html
+      format.xml { render xml:  @kitten }
+      format.json  { render json: @kitten }
+    end
   end
 
   def new
